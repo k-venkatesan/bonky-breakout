1c8a36b68b0e457bcde232c460113c040317b51e
Karthik Venkatesan <karthikvenkatesan.kv@gmail.com>
2020-08-08 22:23:43 +0200

    Add ball
    Add ball that bounces off surfaces; Provide initial force and velocity to ball

---
+++ Assets/Scripts/Gameplay/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Ball that rebounds off surfaces
+/// </summary>
+public class Ball : MonoBehaviour
+{
+    #region Fields
+    #endregion // Fields
+
+    #region Components
+    #endregion // Components
+
+    #region Methods
+
+    /// <summary>
+    /// Apply impulse force to ball
+    /// </summary>
+    private void ApplyImpulseForce()
+    {
+        // Get force magnitude set in configuation file
+        float forceMagnitude = ConfigurationUtils.BallImpulseForce;
+
+        // Set force direction
+        const float ForceAngleInDegrees = 20;
+        float forceAngleInRadians = ForceAngleInDegrees * Mathf.Deg2Rad;
+        Vector2 forceDirection = new Vector2(Mathf.Cos(forceAngleInRadians), Mathf.Sin(forceAngleInRadians));
+
+        // Apply impulse force
+        GetComponent<Rigidbody2D>().AddForce(forceMagnitude * forceDirection, ForceMode2D.Impulse);
+    }
+
+    #endregion // Methods
+
+    #region MonoBehaviour Messages
+
+    private void Start()
+    {
+        ApplyImpulseForce();
+    }
+
+    #endregion // MonoBehaviour Messages
+}
