8bac5bd5b3b6f3aeb835514fc63519fed21da55b
Karthik Venkatesan <karthikvenkatesan.kv@gmail.com>
2020-08-27 10:47:32 +0200

    Prevent collision during spawning of new balls
    Delay spawning of new ball if there is already a collider overlapping the spawn location

--- Assets/Scripts/Gameplay/BallSpawner.cs
+++ Assets/Scripts/Gameplay/BallSpawner.cs
@@ -8,6 +8,10 @@ public class BallSpawner : MonoBehaviour
     [SerializeField]
     private Ball prefabBall;
 
+    // Opposite corners of ball spawn location to check for potential collision
+    private Vector2 ballSpawnLocationBottomLeftCorner;
+    private Vector2 ballSpawnLocationTopRightCorner;
+
     // Timer that allows for a pause before spawning ball
     private Timer waitTimer;
 
@@ -23,6 +27,18 @@ public class BallSpawner : MonoBehaviour
     #endregion // Properties
 
     #region Methods
+
+    /// <summary>
+    /// Calculates bottom-left and top-right points of square area where balls spawn
+    /// </summary>
+    private void CalculateBallSpawnLocationCorners()
+    {
+        CircleCollider2D ballCollider = prefabBall.GetComponent<CircleCollider2D>();
+        ballSpawnLocationBottomLeftCorner.x = ballCollider.transform.position.x - ballCollider.radius;
+        ballSpawnLocationBottomLeftCorner.y = ballCollider.transform.position.y - ballCollider.radius;
+        ballSpawnLocationTopRightCorner.x = ballCollider.transform.position.x + ballCollider.radius;
+        ballSpawnLocationTopRightCorner.y = ballCollider.transform.position.y + ballCollider.radius;
+    }
 
     /// <summary>
     /// Initializes timers that control spawning of balls
@@ -44,7 +60,9 @@ public class BallSpawner : MonoBehaviour
     /// </summary>
     private void MonitorRandomSpawnTimer()
     {
-        if (randomSpawnTimer.Finished)
+        // Check if timer has finished and that there is no object already present in spawn location
+        if (randomSpawnTimer.Finished 
+            && Physics2D.OverlapArea(ballSpawnLocationBottomLeftCorner, ballSpawnLocationTopRightCorner) == null)
         {
             // Spawn ball
             Instantiate(prefabBall);
@@ -57,11 +75,13 @@ public class BallSpawner : MonoBehaviour
 
     /// <summary>
     /// Processes flag that determines if ball is to be spawned or not
-    /// and performs spawning if wait duration is complete
     /// </summary>
     private void ProcessSpawnFlag()
     {
-        if (isBallToBeSpawned && waitTimer.Finished)
+        // Check if a ball is to be spawned, the wait timer has finished, and that there is no object already present in spawn location
+        if (isBallToBeSpawned 
+            && waitTimer.Finished 
+            && Physics2D.OverlapArea(ballSpawnLocationBottomLeftCorner, ballSpawnLocationTopRightCorner) == null)
         {
             Instantiate(prefabBall);
             isBallToBeSpawned = false;
@@ -84,6 +104,7 @@ public class BallSpawner : MonoBehaviour
     private void Start()
     {
         InitializeTimers();
+        CalculateBallSpawnLocationCorners();
     }
 
     private void Update()
