7ec4e2dfc4113cf7c2f72634131ab0467387d972
Karthik Venkatesan <karthikvenkatesan.kv@gmail.com>
2021-01-02 18:44:42 +0100

    Add main menu
    Add main menu from which game can be played or quit

---
+++ Assets/Scripts/Menu/HelpCanvas.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Help canvas opened from main menu
+/// </summary>
+public class HelpCanvas : MonoBehaviour
+{
+    #region Fields
+    #endregion // Fields
+
+    #region Properties
+    #endregion // Properties
+
+    #region Methods
+
+    /// <summary>
+    /// Handles response to 'Back' button being clicked
+    /// </summary>
+    public void HandleBackButtonClick()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+        Destroy(gameObject);
+    }
+
+    #endregion // Methods
+
+    #region MonoBehaviour Messages
+    #endregion // MonoBehaviour Messages
+}
---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Main menu of game
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    #region Fields
+    #endregion // Fields
+
+    #region Properties
+    #endregion // Properties
+
+    #region Methods
+
+    /// <summary>
+    /// Handles response to 'Help' button being clicked
+    /// </summary>
+    public void HandleHelpButtonClick()
+    {
+        gameObject.SetActive(false);
+        Instantiate(Resources.Load("HelpCanvas"));
+    }
+
+    /// <summary>
+    /// Handles response to 'Play' button being clicked
+    /// </summary>
+    public void HandlePlayButtonClick()
+    {
+        SceneManager.LoadSceneAsync("GamePlay");
+    }
+
+    /// <summary>
+    /// Handles response to 'Quit' button being clicked
+    /// </summary>
+    public void HandleQuitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    #endregion // Methods
+
+    #region MonoBehaviour Messages
+    #endregion // MonoBehaviour Messages
+}
